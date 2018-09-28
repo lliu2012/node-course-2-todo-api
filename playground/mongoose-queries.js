78259387b1e6d716c3f7635566443e4ab3296c24
Lei Liu <leiliu525@gmail.com>
2018-09-28 10:15:35 -0500

    Add queries playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,42 @@
+const { ObjectID } = require('mongodb');
+
+const { mongoose } = require('./../server/db/mongoose');
+const { Todo } = require('./../server/model/todo');
+const { User } = require('./../server/model/user');
+
+var id = '5bad304abae44d20213a9c57';
+
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then(todos => {
+//   console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//   _id: id
+// }).then(todo => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id)
+//   .then(todo => {
+//     if (!todo) {
+//       return console.log('ID not found');
+//     }
+//     console.log('Todo by ID', todo);
+//   })
+//   .catch(e => console.log(e));
+
+User.findById(id)
+  .then(user => {
+    if (!user) {
+      return console.log('User not found');
+    }
+
+    console.log(JSON.stringify(user, undefined, 2));
+  })
+  .catch(e => console.log(e));
