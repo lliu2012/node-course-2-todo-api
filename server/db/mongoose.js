3dae8829164b87396bfd57504abcca8179d4abae
Lei Liu <leiliu525@gmail.com>
2018-09-28 18:09:38 -0500

    test connect

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',
+  { useMongoClient: true }
 );
 
 module.exports = {
