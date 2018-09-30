686b89a0796d18f872f21f60ef832c2742533f81
Lei Liu <leiliu525@gmail.com>
2018-09-30 10:29:03 -0500

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var { ObjectId } = require('mongodb');
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const { ObjectId } = require('mongodb');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./model/todo');
@@ -73,6 +74,34 @@ app.delete('/todos/:id', (req, res) => {
       res.send({ todo });
     })
     .catch(e => res.status(400).send());
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({ todo });
+    })
+    .catch(e => {
+      return res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
