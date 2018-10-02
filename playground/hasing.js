3cfc0dc358cfe082925140040d6a909d5ca174fa
Lei Liu <leiliu525@gmail.com>
2018-10-02 15:07:57 -0500

    Hash user password before saving

--- playground/hasing.js
+++ playground/hasing.js
@@ -1,17 +1,33 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, 'abc123');
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-console.log(token);
+var hashedPassword =
+  '$2a$10$wevBOVqzEUTKHeXTbt2Pf.ir8/iFciW/UTJr95MyWkUkRzh3epJ3a';
 
-var decoded = jwt.verify(token, 'abc123');
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
 
-console.log('decoded', decoded);
+// var data = {
+//   id: 10
+// };
+
+// var token = jwt.sign(data, 'abc123');
+
+// console.log(token);
+
+// var decoded = jwt.verify(token, 'abc123');
+
+// console.log('decoded', decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
